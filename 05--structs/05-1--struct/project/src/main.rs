f9be7966e90cc0ed6ca9d548e67268c31a7b5e12
ax-va <vasiliev.informatik@t-online.de>
2025-01-07 22:07:57 +0100

    update

---
+++ 05--structs/05-1--struct/project/src/main.rs
@@ -0,0 +1,55 @@
+/*
+```
+$ cd 05*
+$ cd 05-1*
+$ cargo new project
+$ cd project
+```
+ */
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(username: String, email: String) -> User {
+    User {
+        active: true,
+        username, // field init shorthand
+        email, // field init shorthand
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("ax-va"),
+        email: String::from("ax-va@example.com"),
+        sign_in_count: 1,
+    };
+
+    println!("{0}", user1.username);
+    // ax-va
+
+    // mutable struct
+    let mut user2 = User {active: true,
+        username: String::from("ax-va"),
+        email: String::from("ax-va@example.com"),
+        sign_in_count: 1,
+    };
+
+    // Notation:
+    // The entire instance must be mutable.
+    // Marking only certain fields as mutable is not allowed.
+
+    user2.username = String::from("alex-v");
+    println!("{0}", user2.username);
+    // alex-v
+
+    let user3 = build_user(String::from("axx-va"), String::from("ax-va@example.com"));
+    println!("{0}", user3.username);
+    // axx-va
+}
