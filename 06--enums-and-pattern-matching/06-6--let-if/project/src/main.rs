cdeb36099795804f916c3725b79cdd24a8c5e017
ax-va <vasiliev.informatik@t-online.de>
2025-01-18 23:19:13 +0100

    update

---
+++ 06--enums-and-pattern-matching/06-6--let-if/project/src/main.rs
@@ -0,0 +1,61 @@
+/*
+
+The `if let` syntax lets us handle values that match one pattern while ignoring the rest.
+The code in the `if let` block isn't run if the value doesn't match the pattern.
+We can include an `else` with an `if let`.
+
+```
+$ cd 06*
+$ cd 06-6*
+$ cargo new project
+$ cd project
+```
+ */
+
+ #[derive(Debug)] // to inspect the state
+ enum UsState {
+    Alabama,
+    Alaska,
+    // other US States
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let config_max = Some(3u8);
+
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {max}"),
+        _ => (),
+    }
+    // The maximum is configured to be 3
+
+    // the same behaviour, `max` is used in the body
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {max}");
+    }
+    // The maximum is configured to be 3
+
+    let coin = Coin::Quarter(UsState::Alaska);
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+    // State quarter from Alaska!
+
+    let coin = Coin::Quarter(UsState::Alaska);
+    let mut count = 0;
+    // the same behaviour
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+    // State quarter from Alaska!
+}
