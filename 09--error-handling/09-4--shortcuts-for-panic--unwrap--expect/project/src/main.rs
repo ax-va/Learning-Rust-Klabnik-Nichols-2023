f0b415276a5b7fcb2e4170387db2ac95e55b12f8
ax-va <vasiliev.informatik@t-online.de>
2025-05-02 23:11:21 +0200

    update

---
+++ 09--error-handling/09-4--shortcuts-for-panic--unwrap--expect/project/src/main.rs
@@ -0,0 +1,38 @@
+/*
+```
+$ cd 09*
+$ cd 09-4*
+$ cargo new project
+$ cd project
+$ cargo run
+```
+ */
+use std::fs::File;
+
+fn main() {
+    // If the `Result` value is the `Ok` variant,
+    // `unwrap` will return the value inside the `Ok`.
+    // If the `Result` is the `Err` variant,
+    // `unwrap` will call the `panic!` macro.
+
+    // let greeting_file = File::open("hello.txt").unwrap();
+    /*
+    thread 'main' panicked at src/main.rs:18:49:
+    called `Result::unwrap()` on an `Err` value: Os { code: 2, kind: NotFound, message: "No such file or directory" }
+    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+     */
+
+    // The `expect` method returns the file handle or calls the `panic!` macro
+    // but lets us also choose the `panic!` error message.
+
+    let greeting_file = File::open("hello.txt")
+        .expect("`hello.txt` should be included in this project.");
+    /*
+    thread 'main' panicked at src/main.rs:29:10:
+    `hello.txt` should be included in this project.: Os { code: 2, kind: NotFound, message: "No such file or directory" }
+    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+     */
+
+    // In production-quality code, most Rustaceans choose `expect` over `unwrap`
+    // and give more context about why the operation is expected to always succeed.
+}
