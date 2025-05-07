3e453fd9e825f5f39cc0bb96b86a8b141591709f
ax-va <vasiliev.informatik@t-online.de>
2025-05-07 19:58:21 +0200

    update

--- 09--error-handling/09-5--propagating-errors--Result--question-mark-operator/project/src/main.rs
+++ 09--error-handling/09-5--propagating-errors--Result--question-mark-operator/project/src/main.rs
@@ -68,7 +68,7 @@ fn read_username_from_file_v2() -> Result<String, io::Error> {
     /*
     Err(err) => return Err(From::from(err)),
      */
-    // We need also for converting the error types
+    // We need also this for converting the error types
     /*
     impl From<io::Error> for SomeOtherError
      */
@@ -92,10 +92,73 @@ fn read_username_from_file_v4() -> Result<String, io::Error> {
     fs::read_to_string("hello.txt")
 }
 
+// We can only use the ? operator in a function
+// that returns `Result`, `Option`, or another type that implements the `FromResidual` trait.
+
+// The `?` operator called on an `Option<T>`:
+// - if the value is `None`, the `None` will be returned early from the function at that point;
+// - if the value is `Some`, the value inside the `Some` is the resultant value of the expression,
+// and the function continues.
+
+// Example: find the last character of the first line in the given text
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    // This is an `Option` because it is possible
+    // that the first line is the empty string as in "\nHi!".
+
+    // The `lines` method returns an iterator over the lines in the string.
+    // - If `text` is the empty string, `next` will return `None`,
+    // in which case `?` stops the chain and return `None`.
+    // - If `text` is not the empty string, `next` will return a `Some` value
+    // containing a string slice of the first line in text.
+    // In this case, `chars` on that string slice gets an iterator of its characters.
+    // Then, `last` returns the last item in the iterator.
+    text.lines().next()?.chars().last()
+}
+
+/*
 fn main() {
     // All the calls return the same result
     read_username_from_file_v1();
     read_username_from_file_v2();
     read_username_from_file_v3();
     read_username_from_file_v4();
+
+    // misusing the ? operator
+
+    // compilation error:
+    // "error[E0277]: the `?` operator can only be used in a function that returns `Result` or `Option` (or another type that implements `FromResidual`)"
+    // let greeting_file = File::open("hello.txt")?;
+    //                                            ^ cannot use the `?` operator in a function that returns `()`
+
+    // Explanation:
+    // The `?` operator follows the `Result` value returned by `File::open`,
+    // but this main function returns the `()` type (the *unit type*), not `Result´.
+    // We can only use the `?` operator in a function
+    // that returns `Result`, `Option`, or another type
+    // that implements the `FromResidual` trait.
+}
+*/
+
+// Fix that changing the `main` function.
+
+// This trait represents any error type in Rust.
+// It allows you to work with different kinds of errors in a generic way.
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // `()` is the *unit type*, representing "no meaningful value" like `void` in other languages.
+    // `Box<dyn Error>` means "any kind of error".
+
+    let greeting_file = File::open("hello.txt")?; // returns a `Result<File, std::io::Error>`
+    // - If the file opens successfully, the `File` object is stored in `greeting_file`.
+    // - If there is an error (e.g., file not found),
+    // the error is automatically returned from `main` wrapped in the `Box<dyn Error>`.
+
+    // `Ok(())` is needed at the end of this main function
+    // because the function is declared to return a `Result<(), Box<dyn Error>>`.
+    // So `Ok(())` means "everything went fine, and there's nothing to return".
+    Ok(())
+    // The main function may return any types
+    // that implement the `std::process::Termination` trait,
+    // which contains a function `report` that returns an `ExitCode`.
 }
