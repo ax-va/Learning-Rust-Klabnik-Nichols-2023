9b6d98b656a8e77d69cc8ad4252292b9723d884c
ax-va <vasiliev.informatik@t-online.de>
2025-06-29 19:14:00 +0200

    update

---
+++ 13--closures-and-iterators/13-5--iterators--iter--into_iter--iter_mut--next/project/src/lib.rs
@@ -0,0 +1,21 @@
+/*
+Run tests
+$ cargo test
+ */
+
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+    // Calling the `next` method on an iterator changes internal state
+    // that the iterator uses to keep track of where it is in the sequence,
+    // so we need to make the iterator variable mutable.
+    let mut v1_iter = v1.iter();
+    // Notice:
+    // We didn't need to make `v1_iter` mutable when we used a `for` loop
+    // because the loop took ownership of `v1_iter` and made it mutable behind the scenes.
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
