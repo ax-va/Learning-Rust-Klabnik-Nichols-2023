a8d0526362f557992abc86ccd211acae422bc03f
ax-va <vasiliev.informatik@t-online.de>
2025-08-09 20:04:03 +0200

    update

--- 16--smart-pointers/16-1--boxes/project/src/main.rs
+++ 16--smart-pointers/16-1--boxes/project/src/main.rs
@@ -5,5 +5,20 @@ fn main() {
     let b = Box::new(5);
     println!("b = {b}");
     // b = 5
+
+    let x = 5;
+    // Make a reference to `x`
+    let y = &x;
+    // Use an instance of a box pointing to a copied value of `x`
+    // rather than a reference pointing to the value of `x`.
+    let z = Box::new(x);
+
+    assert_eq!(5, x);
+    // Use the dereference operator `*`
+    assert_eq!(5, *y);
+    // compilation error: "error[E0277]: can't compare `{integer}` with `&{integer}`"
+    // assert_eq!(5, y);
+    assert_eq!(5, *z);
+
 } // The deallocation happens both for the box (stored on the stack)distraction.
 // and the data it points to (stored on the heap).
