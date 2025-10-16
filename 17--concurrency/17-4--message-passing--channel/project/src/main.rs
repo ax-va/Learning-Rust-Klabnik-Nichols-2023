8e3b576890af548ea801f92946a7cf5917f346de
ax-va <vasiliev.informatik@t-online.de>
2025-10-16 22:31:38 +0200

    update

---
+++ 17--concurrency/17-4--message-passing--channel/project/src/main.rs
@@ -0,0 +1,8 @@
+/*
+The idea of *message passing* in a slogan at https://golang.org/doc/effective_go.html#concurrency
+"Do not communicate by sharing memory; instead, share memory by communicating."
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
