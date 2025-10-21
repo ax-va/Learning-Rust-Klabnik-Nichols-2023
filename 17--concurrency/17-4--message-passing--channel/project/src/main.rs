dfa9040853b18ea64bd7ab5fc7607fea93b7447f
ax-va <vasiliev.informatik@t-online.de>
2025-10-21 08:19:25 +0200

    update

--- 17--concurrency/17-4--message-passing--channel/project/src/main.rs
+++ 17--concurrency/17-4--message-passing--channel/project/src/main.rs
@@ -1,8 +1,41 @@
 /*
 The idea of *message passing* in a slogan at https://golang.org/doc/effective_go.html#concurrency
 "Do not communicate by sharing memory; instead, share memory by communicating."
+
+To enable message-passing concurrency, Rust's standard library provides *channels*:
+- A channel is a common concurrency abstraction, in which one thread sends data and another thread receives it.
+- A channel has two halves: a transmitter and a receiver.
+- A channel is closed if the receiver is dropped, or if all of its transmitters are dropped.
  */
 
+use std::sync::mpsc; // mpsc = *multiple producer, single consumer*
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel(); // (tx, rx) = (transmitter, receiver)
+
+    // Create a new thread and then
+    // move the transmitter `tx` into the closure
+    // so that the spawned thread owns `tx`.
+    // The spawned thread needs to own the transmitter
+    // to be able to send messages through the channel.
+    thread::spawn(move || {
+        // The `send` method returns a `Result<T, E>` type.
+        // Use `unwrap` to panic in case of an error.
+        tx.send(String::from("hi")).unwrap();
+        tx.send(String::from("again")).unwrap();
+    });
+
+    // Get the value from the receiver in the main thread:
+    // - The main thread will pause at `recv()` and wait.
+    // - The `recv` method returns a `Result<T, E>` type.
+    // - If a value arrives, `unwrap()` extracts it.
+    // - If the channel is closed before any value is received, `unwrap()` will panic.
+    // - See also `try_recv` that immediately returns `Result`, without blocking the current thread.
+    let received = rx.recv().unwrap(); // recv = receive
+    println!("Got: {received}");
+    // Got: hi
+    let received = rx.recv().unwrap();
+    println!("Got: {received}");
+    // Got: again
 }
