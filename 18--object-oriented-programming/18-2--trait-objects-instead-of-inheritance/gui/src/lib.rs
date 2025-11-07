4bb50ed469ee82678cfd06af03a3083e07127e0e
ax-va <vasiliev.informatik@t-online.de>
2025-11-07 19:11:04 +0100

    update

---
+++ 18--object-oriented-programming/18-2--trait-objects-instead-of-inheritance/gui/src/lib.rs
@@ -0,0 +1,26 @@
+// A *trait* in Rust is like an *interface* in other languages:
+// - It defines behavior that types can implement.
+// - Any type that wants to be "drawable" must implement `fn draw(&self)`.
+pub trait Draw {
+    fn draw(&self);
+}
+
+// `Box<dyn Draw>`:
+// - `dyn Draw` means a *trait object*:
+// a value whose concrete type is unknown at compile time
+// but which implements the `Draw` trait.
+// - `Box<dyn Draw>` means allocating that trait object on the heap and storing a pointer to it.
+
+// As a result:
+// - Rust does not know the exact type inside the `Box` at compile time.
+// - But it does know it implements `Draw`, so it can call `draw` on it at runtime.
+
+// Vectors:
+// - A vector needs to know the size of its elements.
+// - Trait objects (`dyn Draw`) have no known size.
+// - `Box<dyn Draw>` has a known size (a pointer), so it works.
+
+// *Polymorphism* lets us treat different drawable components uniformly and call `draw` on all of them.
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
