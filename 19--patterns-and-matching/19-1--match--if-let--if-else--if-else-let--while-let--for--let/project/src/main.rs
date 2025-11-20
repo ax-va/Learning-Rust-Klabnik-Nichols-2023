d9529f773089f9c2d5fe551fb43f1660d0b05a42
ax-va <vasiliev.informatik@t-online.de>
2025-11-20 20:09:29 +0100

    update

--- 19--patterns-and-matching/19-1--match--if-let--if-else--if-else-let--while-let--for--let/project/src/main.rs
+++ 19--patterns-and-matching/19-1--match--if-let--if-else--if-else-let--while-let--for--let/project/src/main.rs
@@ -56,5 +56,55 @@ fn main() {
     }
     // Using purple as the background color
 
+    /* ----------- */
+    /* `while let` */
+    /* ----------- */
 
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    // The `while let` loop continues
+    // running the code in its block
+    // as long as `pop` returns `Some`.
+    // When `pop` returns `None`, the loop stops.
+    while let Some(top) = stack.pop() {
+        println!("top = {top}");
+    }
+    // top = 3
+    // top = 2
+    // top = 1
+
+    /* ----- */
+    /* `for` */
+    /* ----- */
+
+    let v = vec!['a', 'b', 'c'];
+    // Use a pattern in the `for` loop to destructure tuples
+    for (index, value) in v.iter().enumerate() {
+        println!("index = {index}, value = {value}");
+    }
+    // index = 0, value = a
+    // index = 1, value = b
+    // index = 2, value = c
+
+    /* ----- */
+    /* `let` */
+    /* ----- */
+
+    let (x, y, z) = (1, 2, 3);
+    println!("x = {x}, y = {y}, z = {z}");
+    // x = 1, y = 2, z = 3
+
+    // compilation error: "error[E0308]: mismatched types"
+    // let (x, y) = (1, 2, 3);
+    //     ^^^^^^   --------- this expression has type `({integer}, {integer}, {integer})
+    //     |
+    //     expected a tuple with 3 elements, found one with 2 elements
+
+    // Ignore one or more of the values in the tuple using `..`
+    let (x, y, ..) = (1, 2, 3, 4);
+    println!("x = {x}, y = {y}");
+    // x = 1, y = 2
 }
