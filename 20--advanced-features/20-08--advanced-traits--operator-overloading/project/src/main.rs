ec87ba9068b1781e75e065d016e142796176c30f
ax-va <vasiliev.informatik@t-online.de>
2026-02-19 19:45:26 +0100

    update

--- 20--advanced-features/20-08--advanced-traits--operator-overloading/project/src/main.rs
+++ 20--advanced-features/20-08--advanced-traits--operator-overloading/project/src/main.rs
@@ -6,12 +6,28 @@ using the `Add` trait, associated types, and default generic type parameters.
 // The `Add` trait defines how the `+` operator works
 use std::ops::Add;
 
+#[derive(Debug, Copy, Clone, PartialEq)]
 // The derive attributes automatically implement:
 // - `Debug` -> allows printing
 // - `Copy` -> values are copied instead of moved
+/*
+let p1 = Point { x: 1, y: 2 };
+let p2 = p1; // copied
+ */
+// `Copy` isn't required by `Add`,
+// but when a type implements `Copy`,
+// using the `+` operator duplicates the operands
+// instead of moving them, allowing them to be used afterward.
 // - `Clone` -> can be duplicated
+// `#[derive(Clone)]` auto-generates `impl Clone for Point`.
+// So now we can do:
+/*
+let p1 = Point { x: 1, y: 2 };
+let p2 = p1.clone();
+ */
+// `Clone` is included only because `Copy` requires `Clone`.
+// Rust doesn't automatically derive dependent traits.
 // - `PartialEq` -> allows `==` and `assert_eq!`
-#[derive(Debug, Copy, Clone, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
@@ -52,7 +68,8 @@ fn main() {
         Point { x: 3, y: 3 }
     );
     // `add(self, ...)` takes ownership of self.
-    // Without `Copy`, using `a + b` would move a.
+    // Without `Copy`, using `a + b` would consume `a` and `b`
+    // (move `a` and `b` into the `add` method) -> We could not use them anymore.
     // With `Copy`, it just duplicates it.
 
     assert_eq!(
@@ -61,8 +78,9 @@ fn main() {
     );
 }
 
-// The default generic type in this code is within the `Add` trait.
-// `Rhs` means "right-hand side" in the default generic parameter `<Rhs=Self>`.
+// - The default generic type in this code is within the `Add` trait.
+// - `Rhs` means "right-hand side" in the default generic parameter `<Rhs=Self>`.
+// - `self` in `add` means that the left operand should be moved into the function.
 /*
 trait Add<Rhs=Self> {
     type Output;
