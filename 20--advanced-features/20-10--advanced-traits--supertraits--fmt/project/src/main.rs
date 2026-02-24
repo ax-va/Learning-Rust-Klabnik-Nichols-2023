e2c362293a28ec37d4bd3949c6bc25e1f42e8c28
ax-va <vasiliev.informatik@t-online.de>
2026-02-24 09:38:21 +0100

    update

---
+++ 20--advanced-features/20-10--advanced-traits--supertraits--fmt/project/src/main.rs
@@ -0,0 +1,44 @@
+use std::fmt; // format
+
+// The `Display` trait is a *supertrait* for the `OutlinePrint` trait
+// because `OutlinePrint` uses a method from `Display`.
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        // The `to_string` method is from the `Display` trait
+        let output = self.to_string();
+        let len = output.len();
+
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// The `Point` struct implements the `OutlinePrint` trait
+impl OutlinePrint for Point {}
+
+// The `Point` struct must also implement the `Display` trait
+// because `OutlinePrint` relies on `Display`.
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+fn main() {
+    let point = Point { x: 1, y: 2 };
+
+    point.outline_print();
+    // **********
+    // *        *
+    // * (1, 2) *
+    // *        *
+    // **********
+}
