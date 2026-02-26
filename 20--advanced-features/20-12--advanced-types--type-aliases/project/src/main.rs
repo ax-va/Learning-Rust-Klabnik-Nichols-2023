69884c78af555c3274f4a0be710a6ce977d045f6
ax-va <vasiliev.informatik@t-online.de>
2026-02-26 10:06:20 +0100

    update

---
+++ 20--advanced-features/20-12--advanced-types--type-aliases/project/src/main.rs
@@ -0,0 +1,29 @@
+/*
+The main use case for type aliases is to reduce repetition of lengthy types.
+ */
+
+// Example 1
+
+fn main() {
+    // Create the alias `Kilometers` to `i32`
+    type Kilometers = i32;
+    // Values that have the type `Kilometers`
+    // will be treated the same as values of type `i32`.
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+    println!("x + y = {}", x + y);
+    // x + y = 10
+}
+
+// Example 2
+
+use std::fmt;
+
+type Result<T> = std::result::Result<T, std::io::Error>;
+
+pub trait Write {
+    fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+    fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
+}
